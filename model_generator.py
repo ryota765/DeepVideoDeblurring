73944f63dd9036f8978c1ea4d074ac5c6601af34
Ryota <ryota15765@icloud.com>
2020-07-10 17:53:42 +0900

    black formatting

--- model_generator.py
+++ model_generator.py
@@ -3,8 +3,7 @@ from keras.layers import *
 from keras.models import Model
 
 
-class ModelGenerator():
-
+class ModelGenerator:
     @staticmethod
     def crop(dimension, start, end):
         # Crops (or slices) a Tensor on a given dimension from start to end
@@ -12,105 +11,111 @@ class ModelGenerator():
         # call slice(2, 5, 10) as you want to crop on the second dimension
         def func(x):
             if dimension == 0:
-                return x[start: end]
+                return x[start:end]
             if dimension == 1:
-                return x[:, start: end]
+                return x[:, start:end]
             if dimension == 2:
-                return x[:, :, start: end]
+                return x[:, :, start:end]
             if dimension == 3:
-                return x[:, :, :, start: end]
+                return x[:, :, :, start:end]
             if dimension == 4:
-                return x[:, :, :, :, start: end]
-        return Lambda(func)
+                return x[:, :, :, :, start:end]
 
+        return Lambda(func)
 
     def model(self, n_stack=5, input_size=128, epsilon=0.001):
         # _ shows output after activation layer
 
-        inputs = Input(shape=(input_size, input_size, 3*n_stack)) # (height, width, channel*stack)
-        f0_1 = Conv2D(15, kernel_size=5, padding='same')(inputs)
+        inputs = Input(
+            shape=(input_size, input_size, 3 * n_stack)
+        )  # (height, width, channel*stack)
+        f0_1 = Conv2D(15, kernel_size=5, padding="same")(inputs)
         f0_1 = BatchNormalization(epsilon=epsilon)(f0_1)
-        f0_1_ = Activation('relu')(f0_1) # ReLU(f0)
-        f0_2 = Conv2D(64, kernel_size=3, padding='same')(f0_1_) # Add this layer due to tensor shape
+        f0_1_ = Activation("relu")(f0_1)  # ReLU(f0)
+        f0_2 = Conv2D(64, kernel_size=3, padding="same")(
+            f0_1_
+        )  # Add this layer due to tensor shape
         f0_2 = BatchNormalization(epsilon=epsilon)(f0_2)
-        f0_2_ = Activation('relu')(f0_2)
+        f0_2_ = Activation("relu")(f0_2)
 
-        d1 = Conv2D(64, kernel_size=3, strides=2, padding='same')(f0_2_)
+        d1 = Conv2D(64, kernel_size=3, strides=2, padding="same")(f0_2_)
         d1 = BatchNormalization(epsilon=epsilon)(d1)
-        d1_ = Activation('relu')(d1)
-        f1_1 = Conv2D(128, kernel_size=3, padding='same')(d1_)
+        d1_ = Activation("relu")(d1)
+        f1_1 = Conv2D(128, kernel_size=3, padding="same")(d1_)
         f1_1 = BatchNormalization(epsilon=epsilon)(f1_1)
-        f1_1_ = Activation('relu')(f1_1)
-        f1_2 = Conv2D(128, kernel_size=3, padding='same')(f1_1_)
+        f1_1_ = Activation("relu")(f1_1)
+        f1_2 = Conv2D(128, kernel_size=3, padding="same")(f1_1_)
         f1_2 = BatchNormalization(epsilon=epsilon)(f1_2)
-        f1_2_ = Activation('relu')(f1_2)
+        f1_2_ = Activation("relu")(f1_2)
 
-        d2 = Conv2D(256, kernel_size=3, strides=2, padding='same')(f1_2_)
+        d2 = Conv2D(256, kernel_size=3, strides=2, padding="same")(f1_2_)
         d2 = BatchNormalization(epsilon=epsilon)(d2)
-        d2_ = Activation('relu')(d2)
-        f2_1 = Conv2D(256, kernel_size=3, padding='same')(d2_)
+        d2_ = Activation("relu")(d2)
+        f2_1 = Conv2D(256, kernel_size=3, padding="same")(d2_)
         f2_1 = BatchNormalization(epsilon=epsilon)(f2_1)
-        f2_1_ = Activation('relu')(f2_1)
-        f2_2 = Conv2D(256, kernel_size=3, padding='same')(f2_1_)
+        f2_1_ = Activation("relu")(f2_1)
+        f2_2 = Conv2D(256, kernel_size=3, padding="same")(f2_1_)
         f2_2 = BatchNormalization(epsilon=epsilon)(f2_2)
-        f2_2_ = Activation('relu')(f2_2)
-        f2_3 = Conv2D(256, kernel_size=3, padding='same')(f2_2_)
+        f2_2_ = Activation("relu")(f2_2)
+        f2_3 = Conv2D(256, kernel_size=3, padding="same")(f2_2_)
         f2_3 = BatchNormalization(epsilon=epsilon)(f2_3)
-        f2_3_ = Activation('relu')(f2_3)
+        f2_3_ = Activation("relu")(f2_3)
 
-        d3 = Conv2D(512, kernel_size=3, strides=2, padding='same')(f2_3_)
+        d3 = Conv2D(512, kernel_size=3, strides=2, padding="same")(f2_3_)
         d3 = BatchNormalization(epsilon=epsilon)(d3)
-        d3_ = Activation('relu')(d3)
-        f3_1 = Conv2D(512, kernel_size=3, padding='same')(d3_)
+        d3_ = Activation("relu")(d3)
+        f3_1 = Conv2D(512, kernel_size=3, padding="same")(d3_)
         f3_1 = BatchNormalization(epsilon=epsilon)(f3_1)
-        f3_1_ = Activation('relu')(f3_1)
-        f3_2 = Conv2D(512, kernel_size=3, padding='same')(f3_1_)
+        f3_1_ = Activation("relu")(f3_1)
+        f3_2 = Conv2D(512, kernel_size=3, padding="same")(f3_1_)
         f3_2 = BatchNormalization(epsilon=epsilon)(f3_2)
-        f3_2_ = Activation('relu')(f3_2)
-        f3_3 = Conv2D(512, kernel_size=3, padding='same')(f3_2_)
+        f3_2_ = Activation("relu")(f3_2)
+        f3_3 = Conv2D(512, kernel_size=3, padding="same")(f3_2_)
         f3_3 = BatchNormalization(epsilon=epsilon)(f3_3)
-        f3_3_ = Activation('relu')(f3_3)
+        f3_3_ = Activation("relu")(f3_3)
 
-        u1 = Conv2DTranspose(256, kernel_size=4, strides=2, padding='same')(f3_3_)
+        u1 = Conv2DTranspose(256, kernel_size=4, strides=2, padding="same")(f3_3_)
         u1 = BatchNormalization(epsilon=epsilon)(u1)
-        s1 = Average()([u1, f2_3]) # Original paper might have intended Sum layer
-        s1_ = Activation('relu')(s1)
-        f4_1 = Conv2D(256, kernel_size=3, padding='same')(s1_)
+        s1 = Average()([u1, f2_3])  # Original paper might have intended Sum layer
+        s1_ = Activation("relu")(s1)
+        f4_1 = Conv2D(256, kernel_size=3, padding="same")(s1_)
         f4_1 = BatchNormalization(epsilon=epsilon)(f4_1)
-        f4_1_ = Activation('relu')(f4_1)
-        f4_2 = Conv2D(256, kernel_size=3, padding='same')(f4_1_)
+        f4_1_ = Activation("relu")(f4_1)
+        f4_2 = Conv2D(256, kernel_size=3, padding="same")(f4_1_)
         f4_2 = BatchNormalization(epsilon=epsilon)(f4_2)
-        f4_2_ = Activation('relu')(f4_2)
-        f4_3 = Conv2D(256, kernel_size=3, padding='same')(f4_2_)
+        f4_2_ = Activation("relu")(f4_2)
+        f4_3 = Conv2D(256, kernel_size=3, padding="same")(f4_2_)
         f4_3 = BatchNormalization(epsilon=epsilon)(f4_3)
-        f4_3_ = Activation('relu')(f4_3)
+        f4_3_ = Activation("relu")(f4_3)
 
-        u2 = Conv2DTranspose(128, kernel_size=4, strides=2, padding='same')(f4_3_)
+        u2 = Conv2DTranspose(128, kernel_size=4, strides=2, padding="same")(f4_3_)
         u2 = BatchNormalization(epsilon=epsilon)(u2)
         s2 = Average()([u2, f1_2])
-        s2_ = Activation('relu')(s2)
-        f5_1 = Conv2D(128, kernel_size=3, padding='same')(s2_)
+        s2_ = Activation("relu")(s2)
+        f5_1 = Conv2D(128, kernel_size=3, padding="same")(s2_)
         f5_1 = BatchNormalization(epsilon=epsilon)(f5_1)
-        f5_1_ = Activation('relu')(f5_1)
-        f5_2 = Conv2D(64, kernel_size=3, padding='same')(f5_1_)
+        f5_1_ = Activation("relu")(f5_1)
+        f5_2 = Conv2D(64, kernel_size=3, padding="same")(f5_1_)
         f5_2 = BatchNormalization(epsilon=epsilon)(f5_2)
-        f5_2_ = Activation('relu')(f5_2)
+        f5_2_ = Activation("relu")(f5_2)
 
-        u3 = Conv2DTranspose(64, kernel_size=4, strides=2, padding='same')(f5_2_)
+        u3 = Conv2DTranspose(64, kernel_size=4, strides=2, padding="same")(f5_2_)
         u3 = BatchNormalization(epsilon=epsilon)(u3)
-        s3 = Average()([u3, f0_2]) # Change from author implemantation due to tensor shape
-        s3_ = Activation('relu')(s3)
-        f6_1 = Conv2D(15, kernel_size=3, padding='same')(s3_)
+        s3 = Average()(
+            [u3, f0_2]
+        )  # Change from author implemantation due to tensor shape
+        s3_ = Activation("relu")(s3)
+        f6_1 = Conv2D(15, kernel_size=3, padding="same")(s3_)
         f6_1 = BatchNormalization(epsilon=epsilon)(f6_1)
-        f6_1_ = Activation('relu')(f6_1)
-        f6_2 = Conv2D(3, kernel_size=3, padding='same')(f6_1_)
+        f6_1_ = Activation("relu")(f6_1)
+        f6_2 = Conv2D(3, kernel_size=3, padding="same")(f6_1_)
         f6_2 = BatchNormalization(epsilon=epsilon)(f6_2)
-        f6_2_ = Activation('relu')(f6_2)
+        f6_2_ = Activation("relu")(f6_2)
 
-        inputs_mid = self.crop(3,n_stack//2*3,(n_stack//2+1)*3)(inputs)
+        inputs_mid = self.crop(3, n_stack // 2 * 3, (n_stack // 2 + 1) * 3)(inputs)
         s4 = Average()([inputs_mid, f6_2])
-        s4_ = Activation('sigmoid')(s4)
+        s4_ = Activation("sigmoid")(s4)
 
         model = Model(inputs=inputs, outputs=s4_)
-        
+
         return model
--- preprocess.py
+++ preprocess.py
@@ -9,19 +9,19 @@ import pickle
 
 
 def load_image_from_dir(img_dir):
-    input_paths = glob.glob(os.path.join(img_dir, 'input/*.jpg'))
-    gt_paths = glob.glob(os.path.join(img_dir, 'GT/*.jpg'))
+    input_paths = glob.glob(os.path.join(img_dir, "input/*.jpg"))
+    gt_paths = glob.glob(os.path.join(img_dir, "GT/*.jpg"))
     print(img_dir, len(input_paths))
-    
+
     input_list = []
     gt_list = []
-    
+
     for input_path, gt_path in zip(input_paths, gt_paths):
         input_img = np.array(Image.open(input_path))
         input_list.append(input_img)
         gt_img = np.array(Image.open(gt_path))
         gt_list.append(gt_img)
-        
+
     return np.array(input_list), np.array(gt_list)
 
 
@@ -31,36 +31,36 @@ def flip(img):
 
 
 def rotate(img):
-    img_list= [img]
-    
-    for i in range(1,4):
-        img_rot = np.rot90(img,i)
+    img_list = [img]
+
+    for i in range(1, 4):
+        img_rot = np.rot90(img, i)
         img_list.append(img_rot)
-        
+
     return np.array(img_list)
 
 
-def scale(img, scale_list=[2,3,4]):
+def scale(img, scale_list=[2, 3, 4]):
     img_list = [img]
     height, width, _ = img.shape
-    
+
     for scale in scale_list:
         _height = height // scale
         _width = width // scale
         img_resized = cv2.resize(img, (_width, _height))
         img_list.append(img_resized)
-        
+
     return np.array(img_list)
 
 
 # process gt and input simultaneously to align random crop
-def random_crop(input_img, gt_img, crop_seed, crop_size=(128,128)):
-    
+def random_crop(input_img, gt_img, crop_seed, crop_size=(128, 128)):
+
     input_img_list = []
     gt_img_list = []
-    
+
     height, width, _ = input_img.shape
-    
+
     for i in range(len(crop_seed)):
 
         # adjust seed for each iteration to get sequential input data
@@ -75,10 +75,10 @@ def random_crop(input_img, gt_img, crop_seed, crop_size=(128,128)):
 
         input_img_cropped = input_img[top:bottom, left:right, :]
         gt_img_cropped = gt_img[top:bottom, left:right, :]
-        
+
         input_img_list.append(input_img_cropped)
         gt_img_list.append(gt_img_cropped)
-        
+
     return np.array(input_img_list), np.array(gt_img_list)
 
 
@@ -86,7 +86,7 @@ def random_crop(input_img, gt_img, crop_seed, crop_size=(128,128)):
 def augmentation(input_img, gt_img, crop_seed):
     input_img_list = []
     gt_img_list = []
-    
+
     flip_input_img_array = flip(input_img)
     flip_gt_img_array = flip(gt_img)
 
@@ -97,12 +97,14 @@ def augmentation(input_img, gt_img, crop_seed):
             scale_input_img_array = scale(rotate_input_img_array[j])
             scale_gt_img_array = scale(rotate_gt_img_array[j])
             for k in range(scale_input_img_array.shape[0]):
-                crop_input_img_array, crop_gt_img_array = random_crop(scale_input_img_array[k], scale_gt_img_array[k],crop_seed)
+                crop_input_img_array, crop_gt_img_array = random_crop(
+                    scale_input_img_array[k], scale_gt_img_array[k], crop_seed
+                )
                 crop_input_img_array.tolist()
                 input_img_list.extend(crop_input_img_array)
                 crop_gt_img_array.tolist()
                 gt_img_list.extend(crop_gt_img_array)
-                
+
     return np.array(input_img_list), np.array(gt_img_list)
 
 
@@ -111,74 +113,85 @@ def main(data_path, output_dir, n_stack, crop_num):
     train_IDs = []
     test_IDs = []
 
-    movie_paths = glob.glob(os.path.join(data_path, '*'))
-    
+    movie_paths = glob.glob(os.path.join(data_path, "*"))
+
     # test split by author (https://github.com/shuochsu/DeepVideoDeblurring/issues/2)
-    test_paths=[
-        'IMG_0030',
-        'IMG_0049',
-        'IMG_0021',
-        'IMG_0032',
-        'IMG_0033',
-        'IMG_0031',
-        'IMG_0003',
-        'IMG_0039',
-        'IMG_0037',
-        '720p_240fps_2',
+    test_paths = [
+        "IMG_0030",
+        "IMG_0049",
+        "IMG_0021",
+        "IMG_0032",
+        "IMG_0033",
+        "IMG_0031",
+        "IMG_0003",
+        "IMG_0039",
+        "IMG_0037",
+        "720p_240fps_2",
     ]
 
     for movie_id, path in enumerate(movie_paths):
-        
-        movie_name = path.split('/')[-1]
-        
+
+        movie_name = path.split("/")[-1]
+
         if movie_name in test_paths:
             is_train = False
         else:
             is_train = True
-        
+
         movie_id = str(movie_id).zfill(2)
 
         input_array, gt_array = load_image_from_dir(path)
         crop_seed = [np.random.randint(0, 100) for i in range(crop_num)]
 
         for num_id, (input_img, gt_img) in enumerate(zip(input_array, gt_array)):
-            
+
             # TODO: change indexing from 0 to len-n_stack
-            id_flag = False if (num_id <= n_stack//2 or num_id >= (len(input_array) - n_stack//2)) else True
+            id_flag = (
+                False
+                if (
+                    num_id <= n_stack // 2
+                    or num_id >= (len(input_array) - n_stack // 2)
+                )
+                else True
+            )
             num_id = str(num_id).zfill(3)
 
             input_array_aug, gt_array_aug = augmentation(input_img, gt_img, crop_seed)
 
-            for aug_id, (input_img, gt_img) in enumerate(zip(input_array_aug, gt_array_aug)):
-                
+            for aug_id, (input_img, gt_img) in enumerate(
+                zip(input_array_aug, gt_array_aug)
+            ):
+
                 aug_id = str(aug_id).zfill(3)
                 img_id = movie_id + aug_id + num_id
-                
-                np.save(os.path.join(output_dir,'input_data/{}.npy'.format(img_id)), input_img)
-                np.save(os.path.join(output_dir,'gt_data/{}.npy'.format(img_id)), gt_img)
-                
+
+                np.save(
+                    os.path.join(output_dir, "input_data/{}.npy".format(img_id)),
+                    input_img,
+                )
+                np.save(
+                    os.path.join(output_dir, "gt_data/{}.npy".format(img_id)), gt_img
+                )
+
                 if id_flag == True:
                     if is_train == True:
                         train_IDs.append(img_id)
                     else:
                         test_IDs.append(img_id)
 
-    print('train_ids:', len(train_IDs), 'test_ids:', len(test_IDs))
+    print("train_ids:", len(train_IDs), "test_ids:", len(test_IDs))
 
-    f = open(os.path.join(output_dir,'id_list/train_id.pickle'), 'wb')
+    f = open(os.path.join(output_dir, "id_list/train_id.pickle"), "wb")
     pickle.dump(train_IDs, f)
-    
-    f = open(os.path.join(output_dir,'id_list/test_id.pickle'), 'wb')
+
+    f = open(os.path.join(output_dir, "id_list/test_id.pickle"), "wb")
     pickle.dump(test_IDs, f)
 
-                        
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main(
-        data_path = 'data/DeepVideoDeblurring_Dataset/quantitative_datasets',
-        output_dir = 'data',
-        n_stack = 5, # number of frames in a clip
-        crop_num = 10 # number of random crops
+        data_path="data/DeepVideoDeblurring_Dataset/quantitative_datasets",
+        output_dir="data",
+        n_stack=5,  # number of frames in a clip
+        crop_num=10,  # number of random crops
     )
-
-    
